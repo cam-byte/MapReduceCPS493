72132b70ed581bfdc1e52fddd0882a5d66a2b531
cam-byte <57976254+cam-byte@users.noreply.github.com>
2021-05-03 16:15:44 -0400

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,29 +1,69 @@
 package com.MapReduceCovidNB;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import com.MapReduceCovidNB.MapperNB.TokenizerMapper;
 import com.MapReduceCovidNB.ReducerNB.IntSumReducer;
+import com.WordCount.WordCount;
 
 public class MainClass {
-	public static void main(String[] args) throws Exception {
-		org.apache.log4j.BasicConfigurator.configure();
-	  Configuration conf = new Configuration();
-	  Job job = Job.getInstance(conf, "test");
-	  job.setJarByClass(MapperNB.class);
-	  job.setMapperClass(TokenizerMapper.class);
-	  job.setCombinerClass(IntSumReducer.class);
-	  job.setReducerClass(IntSumReducer.class);
-	  job.setOutputKeyClass(Text.class);
-	  job.setOutputValueClass(IntWritable.class);
-	  FileInputFormat.addInputPath(job, new Path(args[0]));
-	  FileOutputFormat.setOutputPath(job, new Path(args[1]));
-	  System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
+	public static class TokenizerMapper
+    extends Mapper<Object, Text, Text, IntWritable>{
+
+	private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                 ) throws IOException, InterruptedException {
+   StringTokenizer itr = new StringTokenizer(value.toString());
+   while (itr.hasMoreTokens()) {
+     word.set(itr.nextToken());
+     context.write(word, one);
+   }
+ }
+}
+
+	public static class IntSumReducer
+    extends Reducer<Text,IntWritable,Text,IntWritable> {
+	private IntWritable result = new IntWritable();
+
+	public void reduce(Text key, Iterable<IntWritable> values,
+                    Context context
+                    ) throws IOException, InterruptedException {
+		int sum = 0;
+		for (IntWritable val : values) {
+			sum += val.get();
+   }
+   result.set(sum);
+   context.write(key, result);
+ }
+}
+
+public static void main(String[] args) throws Exception {
+	org.apache.log4j.BasicConfigurator.configure();
+ Configuration conf = new Configuration();
+ Job job = Job.getInstance(conf, "word count");
+ job.setJarByClass(MainClass.class);
+ job.setMapperClass(TokenizerMapper.class);
+ job.setCombinerClass(IntSumReducer.class);
+ job.setReducerClass(IntSumReducer.class);
+ job.setOutputKeyClass(Text.class);
+ job.setOutputValueClass(IntWritable.class);
+ FileInputFormat.addInputPath(job, new Path(args[0]));
+ FileOutputFormat.setOutputPath(job, new Path(args[1]));
+ System.exit(job.waitForCompletion(true) ? 0 : 1);
+}
+	
 }
